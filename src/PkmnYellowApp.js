f77baf6b3addde6ef5e1247d0a6a8b6a3a110467
Daniel Brakke <brakscript@gmail.com>
2020-05-29 21:48:28 -0500

    change to array of pokemon, add counter

--- src/PkmnYellowApp.js
+++ src/PkmnYellowApp.js
@@ -7,60 +7,71 @@ import './App.css';
 
 function PkmnYellowApp() {
 
-  const [pkmn, setPkmn] = useLocalStorage("pkmn", initialPkmn);
-  // const [pkmn, setPkmn] = useState(initialPkmn);
-  const [spriteSize, setSpriteSize] = useLocalStorage("sprite-size", 2);
+    const [pkmn, setPkmn] = useLocalStorage("pkmn", initialPkmn);
+    // const [pkmn, setPkmn] = useState(initialPkmn);
+    const [spriteSize, setSpriteSize] = useLocalStorage("sprite-size", 2);
 
-  const catchPokemon = id => {
-      let p = pkmn[id];
-      p.caught = !p.caught;
-      setPkmn({[id]: p, ...pkmn});
-  };
+    const catchPokemon = id => {
+        let p = pkmn[id - 1];
+        p.caught = !p.caught;
+        let newPkmn = pkmn;
+        newPkmn[id - 1] = p;
+        setPkmn([...newPkmn]);
+    };
 
-  const resetState = () => {
-      if (window.confirm("You sure, dawg?")) {
-          setPkmn(JSON.parse(JSON.stringify(initialPkmn)));
-      }
-  };
+    const resetState = () => {
+        if (window.confirm("You sure, dawg?")) {
+            setPkmn(JSON.parse(JSON.stringify(initialPkmn)));
+        }
+    };
 
-  return (
-      <div className="pkmn-app">
-          <div className="controls">
-              <div className="option">
-                  <button
-                      className="button is-small"
-                      onClick={resetState}
-                  >
-                      Reset
-                  </button>
-              </div>
-              <div className="option">
-                  <label htmlFor="points">Sprite Size: {spriteSize}</label>
-                  <input
-                      type="range"
-                      className="slider is-fullwidth"
-                      id="sprite-size"
-                      name="sprite-size"
-                      min="1"
-                      max="10"
-                      value={spriteSize}
-                      onChange={e => setSpriteSize(e.target.value)}
-                  />
-              </div>
-          </div>
-          <div className="pkmn-container section">
-              {Object.keys(pkmn).map(pokemonId => {
-                  let pokemon = pkmn[pokemonId];
-                  return <Pokemon
-                      key={pokemon.id}
-                      pokemon={pokemon}
-                      catchPokemon={catchPokemon}
-                      spriteSize={spriteSize}
-                  />
-              })}
-          </div>
-      </div>
-  );
+    // let pkmnCaught = 0;
+    let pkmnCaught = pkmn.reduce((pkmnCaught, pokemon) => {
+        if (!pokemon) return 0;
+        return pokemon.caught === true ? pkmnCaught + 1 : pkmnCaught;
+    }, 0);
+
+    return (
+        <div className="pkmn-app">
+            <div className="controls">
+                <div className="option">
+                    {pkmnCaught} of {pkmn.length}
+                </div>
+                <div className="option">
+                    <button
+                        className="button is-small"
+                        onClick={resetState}
+                    >
+                        Reset
+                    </button>
+                </div>
+                <div className="option">
+                    <label htmlFor="points">Sprite Size: {spriteSize}</label>
+                    <input
+                        type="range"
+                        className="slider is-fullwidth"
+                        id="sprite-size"
+                        name="sprite-size"
+                        min="1"
+                        max="10"
+                        value={spriteSize}
+                        onChange={e => setSpriteSize(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className="pkmn-container section">
+                {pkmn && pkmn.map(pokemon => {
+                    if (!pokemon) return null;
+                    return <Pokemon
+                        key={pokemon.id}
+                        pokemon={pokemon}
+                        catchPokemon={catchPokemon}
+                        spriteSize={spriteSize}
+                    />
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default PkmnYellowApp;
--- src/initialPkmn.js
+++ src/initialPkmn.js
@@ -1,8 +1,8 @@
 
-const initialPkmn = {};
+const initialPkmn = [];
 
 for (let i = 1; i <= 151; i ++) {
-    initialPkmn[i] = {
+    initialPkmn[i - 1] = {
         "id": i,
         "sprite": `/images/${i.toString().padStart(3, '0')}.png`,
         "caught": false
